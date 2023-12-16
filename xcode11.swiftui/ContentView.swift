423ca7d3fbd976f6d7b5c61a0f0b889327091e50
minhnv16 <nguyenminhasp@gmail.com>
2023-12-17 00:34:39 +0700

    init repo xcode11.swiftui

---
+++ xcode11.swiftui/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  xcode11.swiftui
+//
+//  Created by u on 12/16/23.
+//  Copyright © 2023 u. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+        //Button("123")
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
